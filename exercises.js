1706df681f1ffee8a3d922d01d235f4973e312fa
studiozandra <zan@studiozandra.com>
2019-01-16 21:53:14 -1000

    test

--- exercises.js
+++ exercises.js
@@ -13,7 +13,8 @@ Objects in Javascript can be compared to objects in real life. Objects has a col
     
     Console.log the object.
 */
-    
+
+console.log('test');
   
 /*
 2.  Declare a variable named `dog` and create the following properties (key-value pairs) to the object:
